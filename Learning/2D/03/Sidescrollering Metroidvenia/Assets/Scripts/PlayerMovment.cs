9d98689ac882e323afa92863bcf65e1712493e2d
Shai Simel <shaisimel@gmail.com>
2021-10-13 09:57:58 +0300

    Added camera and jump physics

--- Learning/2D/03/Sidescrollering Metroidvenia/Assets/Scripts/PlayerMovment.cs
+++ Learning/2D/03/Sidescrollering Metroidvenia/Assets/Scripts/PlayerMovment.cs
@@ -8,13 +8,25 @@ public class PlayerMovment : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D rigidbody2d;
     Animator animator;
+    CapsuleCollider2D bodyCollider2d;
+    BoxCollider2D feetCollider2d;
+    int groundLayerIndex;
+    int climbingLayerIndex;
+    float playerGravityScale;
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpHeight = 5f;
+    [SerializeField] float climbSpeed = 5f;
     
     // Start is called before the first frame update
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        bodyCollider2d = GetComponent<CapsuleCollider2D>();
+        groundLayerIndex = LayerMask.GetMask("Ground");
+        climbingLayerIndex = LayerMask.GetMask("Climbing");
+        playerGravityScale = rigidbody2d.gravityScale;
+        feetCollider2d = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -22,6 +34,7 @@ public class PlayerMovment : MonoBehaviour
     {
         Run();
         FlipSprite();
+        climbLadder();
     }
 
     void FlipSprite() {
@@ -42,5 +55,26 @@ public class PlayerMovment : MonoBehaviour
 
     bool isPlayerRunning() {
         return Mathf.Abs(rigidbody2d.velocity.x) > Mathf.Epsilon;
+    }
+
+    void climbLadder() {
+        if (feetCollider2d.IsTouchingLayers(climbingLayerIndex)) {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, moveInput.y * climbSpeed);
+            rigidbody2d.gravityScale = 0f;
+        } else {
+            rigidbody2d.gravityScale = playerGravityScale;
+        }
+        animator.SetBool("isClimbing", isClimbingLadder());
+    }
+
+    bool isClimbingLadder() {
+        return feetCollider2d.IsTouchingLayers(climbingLayerIndex) && Mathf.Abs(rigidbody2d.velocity.y)>Mathf.Epsilon;
+    }
+
+    void OnJump(InputValue input) {
+        if (input.isPressed && feetCollider2d.IsTouchingLayers(groundLayerIndex)) {
+            rigidbody2d.velocity += new Vector2(0, jumpHeight);
+     
+        }
     }
 }
