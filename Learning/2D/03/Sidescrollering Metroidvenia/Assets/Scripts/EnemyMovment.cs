3bf6a24c2f3fd560c61bc867bbf95a227bfef53a
Shai Simel <shaisimel@gmail.com>
2021-10-14 10:13:00 +0300

    Added enemy with basic movment

---
+++ Learning/2D/03/Sidescrollering Metroidvenia/Assets/Scripts/EnemyMovment.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovment : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    [SerializeField] float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2d.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        moveSpeed *= -1;
+        transform.localScale = new Vector2(-(Mathf.Sign(rigidbody2d.velocity.x)), transform.localScale.y);
+    }
+}
--- Learning/2D/03/Sidescrollering Metroidvenia/Assets/Scripts/PlayerMovment.cs
+++ Learning/2D/03/Sidescrollering Metroidvenia/Assets/Scripts/PlayerMovment.cs
@@ -16,6 +16,7 @@ public class PlayerMovment : MonoBehaviour
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpHeight = 5f;
     [SerializeField] float climbSpeed = 5f;
+    bool isAlive = true;
     
     // Start is called before the first frame update
     void Awake()
@@ -32,6 +33,8 @@ public class PlayerMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (!isAlive) return;
+
         Run();
         FlipSprite();
         climbLadder();
@@ -44,8 +47,8 @@ public class PlayerMovment : MonoBehaviour
     }
 
     void OnMove(InputValue input) {
+        if (!isAlive) return;
         moveInput = input.Get<Vector2>();
-        //Debug.Log(moveInput);
     }
 
     void Run() {
@@ -72,9 +75,22 @@ public class PlayerMovment : MonoBehaviour
     }
 
     void OnJump(InputValue input) {
+        if (!isAlive) return;
         if (input.isPressed && feetCollider2d.IsTouchingLayers(groundLayerIndex)) {
             rigidbody2d.velocity += new Vector2(0, jumpHeight);
      
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //Debug.Log("colided - " + collision.gameObject.tag);
+        if (collision.gameObject.tag.Equals("Enemy")) {
+            Die();
+        }
+    }
+
+    void Die() {
+        moveInput = new Vector2(0f, 0f);
+        isAlive = false;
     }
 }
